5addc06168373be157a94102d167e36cc028eed3
newt <ysti2019.win2020.st@gmail.com>
2024-06-27 17:45:13 +0900

    add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react-swc';
+import * as path from "path";
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "src"),
+      },
+    },
 })
